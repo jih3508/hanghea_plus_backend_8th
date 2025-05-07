e1dbc7351d3e1c9622ae8d865e4c4af729e27f57
DESKTOP-FV1N1HI\user <whdlrgus32@naver.com>
2025-05-07 20:18:03 +0900

    [FEAT]Simple Lock List 처리 추가

--- 6week/server-java/src/main/java/kr/hhplus/be/server/domain/lock/SimpleLockService.java
+++ 6week/server-java/src/main/java/kr/hhplus/be/server/domain/lock/SimpleLockService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
@@ -22,8 +23,7 @@ public class SimpleLockService implements LockService {
         try {
             log.debug("락 획득 시도: {}", key);
             // SETNX 명령어로 락 획득 시도 (값이 있으면 실패, 없으면 성공)
-            acquired = Boolean.TRUE.equals(redisTemplate.opsForValue()
-                    .setIfAbsent(key, String.valueOf(Thread.currentThread().getId()), leaseTime, timeUnit));
+            acquired = lock(key, leaseTime, timeUnit);
 
             if (!acquired) {
                 throw new IllegalStateException("Failed to acquire Simple lock for key: " + key);
@@ -37,6 +37,36 @@ public class SimpleLockService implements LockService {
             }
         }
 
+    }
+
+    @Override
+    public <T> T executeWithLockList(List<String> keys, Long waitTime, Long leaseTime, TimeUnit timeUnit, Supplier<T> supplier) {
+
+        boolean acquired = false;
+        for(String key : keys) {
+            try {
+
+                log.debug("락 획득 시도: {}", key);
+                // SETNX 명령어로 락 획득 시도 (값이 있으면 실패, 없으면 성공)
+                acquired = lock(key, leaseTime, timeUnit);
+
+                if (!acquired) {
+                    throw new IllegalStateException("Failed to acquire Simple lock for key: " + key);
+                }
+            }finally {
+                // 락 해제
+                if (acquired) {
+                    unLock(key);
+                }
+            }
+        }
+
+        return supplier.get();
+    }
+
+    private boolean lock(String key, Long leaseTime, TimeUnit timeUnit) {
+        return Boolean.TRUE.equals(redisTemplate.opsForValue()
+                .setIfAbsent(key, String.valueOf(Thread.currentThread().getId()), leaseTime, timeUnit));
     }
 
     private void unLock(String key){
